a57f3fbc5d25c82e7e8a9d3aee24905a13b4743f
MathpatiAshwini <110845470+MathpatiAshwini@users.noreply.github.com>
2022-08-16 11:28:12 +0530

    Add files via upload

---
+++ 10inputka sum.py
@@ -0,0 +1,19 @@
+
+# i=1
+# sum=0
+# while i<=10:
+#     user=int(input("enter the number:"))
+#     i=i+1
+#     sum=sum+user
+#     print(sum)
+    
+
+
+a=int(input("enter the number:"))
+i=0
+sum=0
+while i>a:
+    b=int(input("enter the number:"))
+    sum=sum+b
+    i=i+1
+print(sum)
